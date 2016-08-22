0469ca1ec78c7ce1ad06ff34bfb1704a069ecf57
Doug Feldmann <douglas_feldmann@hms.harvard.edu>
2016-08-22 08:32:24 -0400

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyscripting',
+            version='0.0.1',
+            description='Miscellaneous scripting functions',
+            url='http://github.com/dmf24/pyscripting',
+            author='Doug Feldmann',
+            author_email='doug@feldmann.com',
+            license='MIT',
+            packages=['scripting'],
+            zip_safe=False)
